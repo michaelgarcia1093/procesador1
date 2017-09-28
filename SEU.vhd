62a252a7e90656349434b0e8022a7eb4d879fbf3
utp <utp@utp>
2017-09-28 16:54:00 -0500

    Se añadió el SEU

---
+++ SEU.vhd
@@ -0,0 +1,24 @@
+
+
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity SEU is
+    Port ( Imm : in  STD_LOGIC_VECTOR (12 downto 0);
+           Sout : out  STD_LOGIC_VECTOR (31 downto 0) := (others => '0'));
+end SEU;
+
+architecture arq_SEU of SEU is
+
+begin
+	process(Imm)
+		begin
+			if Imm(12) = '0' then 
+				Sout <= "0000000000000000000" & imm;
+			else
+				Sout <= "1111111111111111111" & imm;
+			end if;
+	end process;
+
+end arq_SEU;
+
